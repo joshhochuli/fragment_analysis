5f591d7a8b1848337e452240db57ca1d4c5bc563
Josh Hochuli <jeh176@pitt.edu>
2019-11-20 00:24:50 -0500

    switch naming to ligand

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -312,33 +312,33 @@ def make_interaction_histograms(df, normalize = True):
             output_filename = stem + f"{fragment_name}/{fragment_name}_{interaction_type}_hist.png"
 
             fragment_aa_counts = copy.deepcopy(empty_aa_counts)
-            compound_aa_counts = copy.deepcopy(empty_aa_counts)
+            ligand_aa_counts = copy.deepcopy(empty_aa_counts)
 
             frag = df[
                     (df.fragment_name == fragment_name) &
                     (df.interaction_type == interaction_type) &
                     (df.is_fragment == True)]
 
-            compound = df[
+            ligand = df[
                     (df.fragment_name == fragment_name) &
                     (df.interaction_type == interaction_type) &
                     (df.is_fragment == False)]
 
             frag_count = len(frag.protein_name.unique())
-            compound_count = len(compound.ligand_name.unique())
+            ligand_count = len(ligand.ligand_name.unique())
 
-            if frag_count == 0 and compound_count == 0:
+            if frag_count == 0 and ligand_count == 0:
                 continue
 
             print(output_filename)
 
             for aa in frag.amino_acid:
                 fragment_aa_counts[aa] += 1
-            for aa in compound.amino_acid:
-                compound_aa_counts[aa] += 1
+            for aa in ligand.amino_acid:
+                ligand_aa_counts[aa] += 1
 
             total_frag_count = len(frag.amino_acid)
-            total_ligand_count = len(compound.amino_acid)
+            total_ligand_count = len(ligand.amino_acid)
 
             if(normalize):
 
@@ -347,8 +347,8 @@ def make_interaction_histograms(df, normalize = True):
                             fragment_aa_counts}
 
                 if(total_ligand_count != 0):
-                    compound_aa_counts = {x: compound_aa_counts[x] / total_ligand_count for x in
-                            compound_aa_counts}
+                    ligand_aa_counts = {x: ligand_aa_counts[x] / total_ligand_count for x in
+                            ligand_aa_counts}
 
 
             frag_counts_list = []
@@ -357,10 +357,10 @@ def make_interaction_histograms(df, normalize = True):
             #be sure that amino acid order matches count order
             for aa in aa_names:
                 frag_counts_list.append(fragment_aa_counts[aa])
-                ligand_counts_list.append(compound_aa_counts[aa])
+                ligand_counts_list.append(ligand_aa_counts[aa])
 
-            plt.bar(x_vals - width/2, frag_counts_list, width, label = f"Fragment (N = {frag_count})")
-            plt.bar(x_vals + width/2, ligand_counts_list, width, label = f"Compound (N = {compound_count})")
+            plt.bar(x_vals - width/2, frag_counts_list, width, label = f"Fragment (N = {frag_count})", color = "black")
+            plt.bar(x_vals + width/2, ligand_counts_list, width, label = f"Ligand (N = {ligand_count})", color = "gray")
             plt.xticks(x_vals, aa_names)
             plt.title(f"{fragment_name}, {interaction_type}")
             plt.ylabel("Frequency")
@@ -371,7 +371,7 @@ def make_interaction_histograms(df, normalize = True):
 def main():
 
     df = get_dataframe()
-    make_sequence_histograms(df)
+    make_interaction_histograms(df)
 
 if __name__ == "__main__":
     main()
